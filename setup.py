7e69c180cc49eab3a0e34c944967a31af2bc5577
Aram Petrosyan <4r4m.petrosyan@gmail.com>
2017-02-19 13:31:13 +0400

    Module added in PyPi

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="invader",
+    version="0.1.2",
+    packages=['invader'],
+    url='https://github.com/rootKot/invader',
+    author='Aram Petrosyan',
+    author_email='4r4m.petrosyan@gmail.com',
+)
